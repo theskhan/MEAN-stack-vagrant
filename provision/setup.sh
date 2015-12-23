914d4b761bcce9a2ab7bd9b528cc966f990c4f5a
theskhan <suhelkhan06@gmail.com>
2015-12-23 19:05:04 +0530

    fix vagrant path

--- provision/setup.sh
+++ provision/setup.sh
@@ -11,7 +11,7 @@ apt-get install nginx -y > /dev/null
 
 echo "Configuring Nginx"
 cp /vagrant/provision/config/nginx_vhost /etc/nginx/sites-available/nginx_vhost > /dev/null
-#ln -s /etc/nginx/sites-available/nginx_vhost /etc/nginx/sites-enabled/
+ln -s /etc/nginx/sites-available/nginx_vhost /etc/nginx/sites-enabled/
 rm -rf /etc/nginx/sites-available/default
 service nginx restart > /dev/null
 
